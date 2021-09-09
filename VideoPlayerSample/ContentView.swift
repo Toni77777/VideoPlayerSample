c88470617d798f24ac828c646eb5f2238e3a74cd
Anton Paliakou <toxa95401@gmail.com>
2021-09-09 21:35:39 +0300

    Init project.

---
+++ VideoPlayerSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoPlayerSample
+//
+//  Created by Anton Paliakou on 9/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoPlayerSample/VideoPlayerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerSampleApp.swift
+//  VideoPlayerSample
+//
+//  Created by Anton Paliakou on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
